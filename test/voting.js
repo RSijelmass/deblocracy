6a1ca49b0ccda94d84b919f364bfdac16b094b5d
Charlotte Fereday <cbrfereday@gmail.com>
2017-07-19 14:44:25 +0100

    Adds in Voting spike of test

--- test/voting.js
+++ test/voting.js
@@ -1,1 +1,17 @@
 var Voting = artifacts.require("./Voting.sol");
+
+contract('Voting', function(accounts) {
+  it('should be initialized with three candidates', function() {
+    console.log('hello1')
+    return Voting.deployed().then(function(instance) {
+        console.log('hello2')
+        console.log(instance.candidateList)
+      return instance.call(accounts[0]);
+        console.log('hello3')
+    }).then(function(candidateList){
+        console.log('hello4')
+      assert.equal(candidateList.valueOf(), ['Rama', 'Jose', 'DemocRITA'], "3 candidates not available for voting")
+        console.log('hello5')
+    });
+  });
+});
