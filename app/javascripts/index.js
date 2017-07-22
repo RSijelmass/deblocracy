53941a069160c9f5ced6080322f74e060e0b5ec1
RSijelmass <R.Sijelmass@gmail.com>
2017-07-22 09:30:01 +0100

    (WIP) #voteYes and #voteNo now include #updateTally

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -1,16 +1,31 @@
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-// console.log(data)
-// abi = JSON.parse(data);
-// console.log(abi)
 
-var json=JSON.stringify(data);
-var abi=JSON.parse(json)
+//document.addEventListener('DOMContentLoaded', function() {
 
-ElectionContract = web3.eth.contract(abi);
-contractInstance = ElectionContract.at('0x10922319451f069ad592b3bbee4489821e85cc81');
-candidates = {"yes": "yes", "no": "no"}
+	ElectionContract = web3.eth.contract(data.abi);
+	//deployedContract = ElectionContract.new(['Yes', 'No'], {from: web3.eth.accounts[0], gas: 4700000})
+	//console.log(deployedContract.address)
+	contractInstance = ElectionContract.at('0x3f90d19f535f850ef9535c59f6cdc36f05c0da7d');
 
+	function voteYes() {
+		document.getElementById("msg").innerHTML = "YOU VOTED YAASSSSSS"
+		contractInstance.vote(0, {from: web3.eth.accounts[0]})
+		console.log(contractInstance.vote)
+		console.log(contractInstance.vote(0, {from: web3.eth.accounts[0]}))
+		updateTally(0)
+	}
 
-function voteYes() {
-  document.getElementById("msg").innerHTML = "YOU VOTED YAASSSSSS"
-}
+	function voteNo() {
+		document.getElementById("msg").innerHTML = "You voted!"
+		contractInstance.vote(1, {from: web3.eth.accounts[0]})
+		updateTally(1)
+	}
+
+	function updateTally(candidate) {
+		var tally = contractInstance.getCandidateVotes.call(candidate)
+		console.log(contractInstance.getCandidateVotes);
+		console.log(tally.toNumber())
+		document.getElementById(`${candidate}-votes`).innerHTML = tally;
+	};
+//});
+
