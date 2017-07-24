446b54b6964f9b2f58abb8c5aa2326beb13c46fc
RSijelmass <R.Sijelmass@gmail.com>
2017-07-24 12:26:11 +0100

    Page successfully updates tally from yes / no - contract needs to be deployed each time (using node in terminal) - guard clause at #vote commented out: people don't have to be   registered, and can vote multiple times

--- contracts/NewElection.sol
+++ contracts/NewElection.sol
@@ -64,12 +64,12 @@ contract NewElection {
     function vote(uint candidateID) returns (uint votesForCandidate) {
       Voter currentVoter = voters[msg.sender];
 
-      if (!currentVoter.voted && now < currentElection.deadline && currentVoter.registered) {
+     // if (!currentVoter.voted && now < currentElection.deadline && currentVoter.registered) {
         currentVoter.voted = true;
         currentVoter.votedFor = candidateID;
         currentElection.candidatesList[candidateID].voteCount++;
         Voted(candidateID, msg.sender);
-      }
+     // }
 
       return currentElection.candidatesList[candidateID].voteCount;
     }
