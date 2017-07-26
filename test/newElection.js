9d26db416a047b6a8ca88d3ddab2867d9f0c0567
RSijelmass <R.Sijelmass@gmail.com>
2017-07-26 09:53:02 +0100

    Fixes tests: users need to register before voting

--- test/newElection.js
+++ test/newElection.js
@@ -93,7 +93,9 @@ contract('NewElection', function(accounts) {
 
     return NewElection.new(title, electionPeriod, ['Yes', 'No']).then(function(instance) {
       currentElection = instance;
-      return currentElection.vote(0);
+      return instance.registerVoter(accounts[0]);
+    }).then(function() {
+			return currentElection.vote(0);
     }).then(function() {
       return currentElection.tallyElectionResults();
     }).then(function(winningCandidateID) {
@@ -108,7 +110,9 @@ contract('NewElection', function(accounts) {
 
     return NewElection.new(title, electionPeriod, ['Yes', 'No']).then(function(instance) {
       currentElection = instance;
-      return currentElection.vote(0);
+      return instance.registerVoter(accounts[0]);
+    }).then(function() {
+			return currentElection.vote(0);
     }).then(function() {
       return currentElection.declareWinner();
     }).then(function(NewElectionWinner) {
@@ -138,7 +142,9 @@ contract('NewElection', function(accounts) {
 
     return NewElection.new(title, electionPeriod, ['Yes', 'No']).then(function(instance) {
       currentElection = instance;
-      return currentElection.vote(0);
+      return instance.registerVoter(accounts[0]);
+    }).then(function() {
+			return currentElection.vote(0);
     }).then(function(result) {
       return currentElection.displayOwnVote();
     }).then(function(candidateName) {
