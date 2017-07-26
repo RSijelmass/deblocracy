cd0ac06130ef6a50ef1a5bd56cec80f89470db47
Kavita <kkavita92@gmail.com>
2017-07-26 11:38:03 +0100

    First commit

--- test/voting.js
+++ test/voting.js
@@ -1,1 +1,12 @@
 var Voting = artifacts.require("./Voting.sol");
+
+contract('Voting', function(accounts) {
+  it('should be initialized with three candidates', function() {
+    console.log('hello')
+    return Voting.deployed().then(function(instance) {
+      return instance.candidateList.call(accounts[0]);
+    }).then(function(candidateList){
+      assert.equal(candidateList.valueOf(), ['Rama', 'Jose', 'DemocRITA'], "3 candidates not available for voting")
+    });
+  });
+});
